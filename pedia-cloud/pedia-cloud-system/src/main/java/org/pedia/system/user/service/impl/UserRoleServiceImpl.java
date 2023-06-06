c20c7890a0a652c85345e586813aca17015f5935
laiwurui <850874993@qq.com>
2023-06-06 12:41:10 +0800

    fix bug

--- pedia-cloud/pedia-cloud-system/src/main/java/org/pedia/system/user/service/impl/UserRoleServiceImpl.java
+++ pedia-cloud/pedia-cloud-system/src/main/java/org/pedia/system/user/service/impl/UserRoleServiceImpl.java
@@ -4,6 +4,8 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.pedia.system.user.entity.UserRole;
 import org.pedia.system.user.mapper.UserRoleMapper;
 import org.pedia.system.user.service.IUserRoleService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserRoleServiceImpl extends ServiceImpl<UserRoleMapper, UserRole> implements IUserRoleService {
 }
